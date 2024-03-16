599342d37c67a4b25a5f84b8e3d932769b076439
Ouail-01 <ouaillaarabi@gmail.com>
2024-03-16 03:44:29 +0000

    Install Tailwind & Complete the Header

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#18181B",
+        "primary-2": "#878686",
+        // "primary-3": "#4F4F4F",
+        "secondary": "#4F4F4F",
+        "tertiary": "#fff",
+        "quaternary": "#03132e",
+        // New custom colors
+        customColor1: '#1E293B', // dark-blue
+        customColor2: '#00ff00', // Green
+        customColor3: '#0000ff', // Blue
+      },
+      fontFamily: {
+        opensans: ["Open Sans", "sans-serif"],
+        plusjakartasans: ["Plus Jakarta Sans", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+        mulish: ["Mulish", "sans-serif"],
+      },
+    },
+      screens: {
+        xs: "500px",
+        sm: "778px",
+        md: "1060px",
+      },
+    },
+  plugins: [],
+};
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
